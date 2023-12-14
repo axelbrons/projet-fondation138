f6469de70bbd440996ea48122b7a8744b8a9c0b6
valko <valentin.kocijancic@edu.ece.fr>
2023-12-14 17:13:46 +0100

    pour le petit hugo

--- src/fondation.py
+++ src/fondation.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import imutils
+from testval import ouvrir_image
 # colors
 from webcolors import rgb_to_name, CSS3_HEX_TO_NAMES, hex_to_rgb  # python3 -m pip install webcolors
 from scipy.spatial import KDTree
@@ -68,83 +69,89 @@ class ShapeDetector:
         return shape
 
 
-if __name__ == '__main__':
-    # define a video capture object
-    vid = cv2.VideoCapture(0)
+##if __name__ == '__main__':
+##    # define a video capture object
+##    vid = cv2.VideoCapture(0)
+##
+##    while (True):
+##        # Capture the video frame
+##        # by frame
+##        ret, frame = vid.read()
+##
+##        # Display the resulting frame
+##        cv2.imshow('frame', frame)
+##
+##        # load the image and resize it to a smaller factor so that
+##        # the shapes can be approximated better
+##        resized = imutils.resize(frame, width=300)
+##        ratio = frame.shape[0] / float(resized.shape[0])
+##
+##        # convert the resized image to grayscale, blur it slightly,
+##        # and threshold it
+##        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+##        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+##        thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+##
+##        # find contours in the thresholded image and initialize the
+##        # shape detector
+##        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+##                                cv2.CHAIN_APPROX_SIMPLE)
+##        cnts = imutils.grab_contours(cnts)
+##        sd = ShapeDetector()
+##        # loop over the contours
+##        for c in cnts:
+##            # compute the center of the contour
+##            M = cv2.moments(c)
+##
+##            # Vérifier si M["m00"] n'est pas égal à zéro avant la division
+##            if M["m00"] != 0:
+##                cX = int((M["m10"] / M["m00"]) * ratio)
+##                cY = int((M["m01"] / M["m00"]) * ratio)
+##
+##            # detect shape from contour
+##            shape = sd.detect(c)
+##
+##            # resize the contour
+##            c = c.astype("float")
+##            c *= ratio
+##            c = c.astype("int")
+##            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+##
+##            # draw contour with mask
+##            mask = np.zeros(frame.shape[:2], np.uint8)
+##            cv2.drawContours(mask, [c], -1, 255, -1)
+##            img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+##
+##            # Convert to RGB and get color name
+##            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+##            mean = cv2.mean(imgRGB, mask=mask)[:3]
+##            named_color = convert_rgb_to_names(mean)
+##
+##            # get complementary color for text
+##            mean2 = (255 - mean[0], 255 - mean[1], 255 - mean[2])
+##
+##            # display shape name and color
+##            objLbl = shape + " {}".format(named_color)
+##            textSize = cv2.getTextSize(objLbl, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
+##            cv2.putText(frame, objLbl, (int(cX - textSize[0] / 2), int(cY + textSize[1] / 2)), cv2.FONT_HERSHEY_SIMPLEX,
+##                        0.5, mean2, 2)
+##
+##            # show image
+##            cv2.imshow("Image", frame)
+##
+##        # the 'q' button is set as the
+##        # quitting button you may use any
+##        # desired button of your choice
+##        if cv2.waitKey(1) & 0xFF == ord('q'):
+##            break
+##
+##    # After the loop release the cap object
+##    vid.release()
+##    # Destroy all the windows
+##    cv2.destroyAllWindows()
 
-    while (True):
-        # Capture the video frame
-        # by frame
-        ret, frame = vid.read()
 
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
 
-        # load the image and resize it to a smaller factor so that
-        # the shapes can be approximated better
-        resized = imutils.resize(frame, width=300)
-        ratio = frame.shape[0] / float(resized.shape[0])
 
-        # convert the resized image to grayscale, blur it slightly,
-        # and threshold it
-        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-        thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-
-        # find contours in the thresholded image and initialize the
-        # shape detector
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        sd = ShapeDetector()
-        # loop over the contours
-        for c in cnts:
-            # compute the center of the contour
-            M = cv2.moments(c)
-
-            # Vérifier si M["m00"] n'est pas égal à zéro avant la division
-            if M["m00"] != 0:
-                cX = int((M["m10"] / M["m00"]) * ratio)
-                cY = int((M["m01"] / M["m00"]) * ratio)
-
-            # detect shape from contour
-            shape = sd.detect(c)
-
-            # resize the contour
-            c = c.astype("float")
-            c *= ratio
-            c = c.astype("int")
-            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-
-            # draw contour with mask
-            mask = np.zeros(frame.shape[:2], np.uint8)
-            cv2.drawContours(mask, [c], -1, 255, -1)
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # Convert to RGB and get color name
-            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            mean = cv2.mean(imgRGB, mask=mask)[:3]
-            named_color = convert_rgb_to_names(mean)
-
-            # get complementary color for text
-            mean2 = (255 - mean[0], 255 - mean[1], 255 - mean[2])
-
-            # display shape name and color
-            objLbl = shape + " {}".format(named_color)
-            textSize = cv2.getTextSize(objLbl, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
-            cv2.putText(frame, objLbl, (int(cX - textSize[0] / 2), int(cY + textSize[1] / 2)), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, mean2, 2)
-
-            # show image
-            cv2.imshow("Image", frame)
-
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # After the loop release the cap object
-    vid.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+image =cv2.imread('..//img/WIN_20231205_15_46_24_Pro.jpg')
+ouvrir_image(image)
